cfd68d27f1e9fe121f72021c0cce0ce71e51650d
sxc <s2839846578@163.com>
2022-07-30 11:53:37 +0800

    20220724

--- lightProject/src/main/java/com/light/demo/pojo/Light.java
+++ lightProject/src/main/java/com/light/demo/pojo/Light.java
@@ -2,13 +2,8 @@ package com.light.demo.pojo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class Light {
     private Integer id;
@@ -19,8 +14,14 @@ public class Light {
 
     private Integer frequency;
 
-    private String strengthDW;
+    private String messageFre;
 
-    private String frequencyDW;
+    private String messageStr;
 
+    public Light(Integer lightId, String lightName, Integer strength, Integer frequency) {
+        this.id = lightId;
+        this.name = lightName;
+        this.strength = strength;
+        this.frequency = frequency;
+    }
 }
