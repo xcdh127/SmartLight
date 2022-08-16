f49fdedca15bc785912baba0e8a184085d7fb646
sxc <s2839846578@163.com>
2022-08-16 10:08:20 +0800

    20220816

--- lightProject/src/main/resources/sql/light.sql
+++ lightProject/src/main/resources/sql/light.sql
@@ -30,7 +30,37 @@ CREATE TABLE `light` (
 
 /*Data for the table `light` */
 
-insert  into `light`(`id`,`name`,`strength`,`frequency`) values (1,'\"灯箱1号\"',44,44);
+insert  into `light`(`id`,`name`,`strength`,`frequency`) values (1,'\"灯箱1号\"',0,40);
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `sex` varchar(10) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`user_id`,`user_name`,`password`,`email`,`sex`) values (1,'admin','123456','zhongguo@163.com','男'),(2,'user01','123456','222@qq.com','女');
+
+/*Table structure for table `user_light` */
+
+DROP TABLE IF EXISTS `user_light`;
+
+CREATE TABLE `user_light` (
+  `user_light_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `light_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_light_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `user_light` */
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
