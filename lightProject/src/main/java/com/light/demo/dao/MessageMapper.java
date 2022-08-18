7f6323cfba3cbc7c6b24d370d507b34240f36859
sxc <s2839846578@163.com>
2022-08-18 20:36:40 +0800

    20220818

---
+++ lightProject/src/main/java/com/light/demo/dao/MessageMapper.java
@@ -0,0 +1,17 @@
+package com.light.demo.dao;
+
+import com.light.demo.pojo.Message;
+
+public interface MessageMapper {
+    int deleteByPrimaryKey(Integer messageId);
+
+    int insert(Message record);
+
+    int insertSelective(Message record);
+
+    Message selectByPrimaryKey(Integer messageId);
+
+    int updateByPrimaryKeySelective(Message record);
+
+    int updateByPrimaryKey(Message record);
+}
